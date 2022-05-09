d45b8c0248d24d476ab11c993880d765758c420c
hlassiege <hugo@malt.com>
2022-05-09 18:10:29 +0200

    Remove compile error due to wip page

---
+++ pages/resources.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="dark:bg-gray-900 mt-6">
+    <div class="max-w-screen-lg md:flex mx-auto dark:bg-gray-900">
+      <div class="md:w-1/3 p-2 md:flex md:justify-center">
+        <AuthorCard />
+      </div>
+      <div class="md:w-2/3 px-8">
+        <AuthorIntro></AuthorIntro>
+
+        <Expertise></Expertise>
+        <TimeLine></TimeLine>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import siteMetaInfo from "@/data/sitemetainfo";
+export default {
+  data() {
+    return {
+      siteMetaInfo: siteMetaInfo,
+    };
+  },
+  async asyncData({ $content, params, route }) {
+    const articles = await $content("articles", {deep: true}, params.slug)
+      .only([
+        "title",
+        "description",
+        "img",
+        "slug",
+        "tag",
+        "author",
+        "date",
+        "draft",
+        "path",
+      ])
+      .sortBy("date", "desc")
+      .fetch();
+
+    return {
+      articles,
+    };
+  },
+  head: {
+    title: "A propos de l'auteur | " + siteMetaInfo.title,
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: siteMetaInfo.description,
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+};
+</script>
+
+<style></style>
