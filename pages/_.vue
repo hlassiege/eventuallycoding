e547b019cdcd17caee30f0e2221885ce9fa4a288
hlassiege <hugo@malt.com>
2022-05-12 18:43:48 +0200

    Affichage d'un article de blog

--- pages/_.vue
+++ pages/_.vue
@@ -1,4 +1,18 @@
 <template>
+  <div>
+    <section class="flex items-center justify-center h-128 bg-transparent">
+      <canvas id="c" class="-z-10 absolute w-full h-128 block"></canvas>
+
+      <div class="clip-ellipse absolute top-124  rotate-180">
+      </div>
+
+      <img
+        :src="'/images/covers/'+article.cover"
+        :alt="article.title"
+        class="absolute top-96 w-96 -translate-y-32"
+      />
+
+    </section>
   <div class="px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0 mt-10">
     <p class="text-center font-bold my-5 text-indigo-500">
       {{ formatDate(article.date) }}
@@ -19,7 +33,7 @@
         </div>
         <a
           target="_blank"
-          :href="siteMetadata.twitter"
+          :href="siteMetadata.twitter + siteMetadata.twitter_user"
           class="text-sky-500 hover:text-sky-600 dark:text-sky-400"
         >
           @{{ siteMetadata.twitter_user }}
@@ -31,12 +45,14 @@
       :src="article.image"
     />
 
-    <nuxt-content class="prose lg:prose-xl min-w-full p-10 mx-auto" :document="article" />
+    <nuxt-content class="prose  min-w-full p-10 mx-auto" :document="article" />
+  </div>
   </div>
 </template>
 <script>
 import Prism from "~/plugins/prism";
 import siteMetaInfo from "@/data/sitemetainfo";
+import * as THREE from "three";
 export default {
   data() {
     return {
@@ -58,6 +74,119 @@ export default {
   },
   mounted() {
     Prism.highlightAll();
+    const canvas = document.getElementById("c");
+    document.addEventListener("DOMContentLoaded", () => {
+      canvas.style.opacity = 1;
+    });
+
+    const getRandomParticlePos = (particleCount) => {
+      const arr = new Float32Array(particleCount * 3);
+      for (let i = 0; i < particleCount; i++) {
+        arr[i] = (Math.random() - 0.5) * 10;
+      }
+      return arr;
+    };
+    const resizeRendererToDisplaySize = (renderer) => {
+      const canvas = renderer.domElement;
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      const needResize = canvas.width !== width || canvas.height !== height;
+      // resize only when necessary
+      if (needResize) {
+        //3rd parameter `false` to change the internal canvas size
+        renderer.setSize(width, height, false);
+      }
+      return needResize;
+    };
+
+// mouse
+    let mouseX = 0;
+    let mouseY = 0;
+    document.addEventListener("mousemove", (e) => {
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+    });
+
+    const main = () => {
+      const canvas = document.getElementById("c");
+      const renderer = new THREE.WebGLRenderer({ canvas });
+      renderer.setClearColor(new THREE.Color("#202b3d"));
+      const scene = new THREE.Scene();
+
+      // light source
+      const color = 0xffffff;
+      const intensity = 1;
+      const light = new THREE.DirectionalLight(color, intensity);
+      light.position.set(-1, 2, 4);
+      scene.add(light);
+
+      // camera
+      const fov = 75,
+        aspect = 2,
+        near = 1.5,
+        far = 5;
+      const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+      camera.position.z = 2;
+
+      // Geometry
+      const geometrys = [new THREE.BufferGeometry(), new THREE.BufferGeometry()];
+
+      geometrys[0].setAttribute(
+        "position",
+        new THREE.BufferAttribute(getRandomParticlePos(350), 3)
+      );
+      geometrys[1].setAttribute(
+        "position",
+        new THREE.BufferAttribute(getRandomParticlePos(1500), 3)
+      );
+
+      const loader = new THREE.TextureLoader();
+
+      // material
+      const materials = [
+        new THREE.PointsMaterial({
+          size: 0.05,
+          map: loader.load("/sp1.png"),
+          transparent: true,
+          color: "#0e6594"
+        }),
+        new THREE.PointsMaterial({
+          size: 0.075,
+          map: loader.load("/sp2.png"),
+          transparent: true
+          // color: "#0000ff"
+        })
+      ];
+
+      const starsT1 = new THREE.Points(geometrys[0], materials[0]);
+      const starsT2 = new THREE.Points(geometrys[1], materials[1]);
+      scene.add(starsT1);
+      scene.add(starsT2);
+
+      const render = (time) => {
+        // time *= 0.001; //in seconds
+
+        if (resizeRendererToDisplaySize(renderer)) {
+          const canvas = renderer.domElement;
+          // changing the camera aspect to remove the strechy problem
+          camera.aspect = canvas.clientWidth / canvas.clientHeight;
+          camera.updateProjectionMatrix();
+        }
+
+        starsT1.rotation.y = 0.00005 * time ;
+        starsT1.rotation.x = 0.00005 * time ;
+        starsT2.rotation.y = 0.00005 * time;
+        starsT2.rotation.x = 0.00005 * time;
+
+        // Re-render the scene
+        renderer.render(scene, camera);
+        // loop
+        requestAnimationFrame(render);
+      };
+      requestAnimationFrame(render);
+    };
+    main();
+
   },
   head() {
     return {
@@ -84,5 +213,12 @@ export default {
 }
 .nuxt-content p {
   margin-bottom: 20px;
+}
+.clip-ellipse {
+  clip-path: ellipse(60% 100% at 50% 0%);
+  @apply bg-white;
+  height: 100px;
+  width: 100%;
+
 }
 </style>
