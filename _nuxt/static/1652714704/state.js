6d449f7195584ecc8ef5e9114be0df56c0efef60
hlassiege <h.lassiege@free.fr>
2022-05-16 15:47:16 +0000

    Update cloudflare to output generated at 756af8f

--- _nuxt/static/1652714704/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652714704",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"95ffebfc"}}}}(null,"\u002F"));
