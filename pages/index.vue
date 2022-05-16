210af2c409ff225df92e399ea9065e0be9165b46
hlassiege <hugo@malt.com>
2022-05-16 13:58:59 +0200

    Bouton "en lire plus" sur la Home

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,16 @@
                   :path="article.path"
         />
 
+        <div class="text-center mt-16">
+          <nuxt-link class="relative inline-block group focus:outline-none focus:ring" to="/blog">
+            <span class="absolute inset-0 transition-transform translate-x-1.5 translate-y-1.5 bg-pink-300 group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+            <span class="relative inline-block px-8 py-3 text-sm font-bold tracking-widest text-black uppercase border-2 border-current group-active:text-opacity-75">
+              En lire plus
+            </span>
+            </nuxt-link>
+        </div>
+
 
       </div>
 
@@ -75,7 +85,7 @@ export default {
         "path",
         "cover"
       ])
-      .limit(5)
+      .limit(3)
       .sortBy("date", "desc")
       .fetch();
 
