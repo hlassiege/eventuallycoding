aa4fa0363972f5e5599366b04de8facb4bda27b9
hlassiege <h.lassiege@free.fr>
2022-05-18 17:53:17 +0000

    Update cloudflare to output generated at 1cf875d

--- _nuxt/static/1652866499/HeroSection/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652866499",layout:"default",error:a,serverRendered:true,routePath:"\u002FHeroSection",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a9354dc9"}}}}(null));
