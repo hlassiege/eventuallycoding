b022ae5967cdc880f966441d09da7db4ae3590b4
hlassiege <h.lassiege@free.fr>
2022-05-26 09:54:51 +0000

    Update cloudflare to output generated at 531c449

--- _nuxt/static/1653558808/2013/07/20/eventual-coder/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653558808",layout:"default",error:a,serverRendered:true,routePath:"\u002F2013\u002F07\u002F20\u002Feventual-coder",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"119d65cb"}},_img:{"/_ipx/_/hugo-nb.jpg":"\u002F_nuxt\u002Fimage\u002Fc3e54c.jpg"}}}(null));
