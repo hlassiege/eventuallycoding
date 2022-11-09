dc5a769fc3d2e17deedf11886f583a10e8c25de7
Hugo Lassiège <hugo@malt.com>
2022-11-09 14:14:41 +0100

    Add sitemap

---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,20 @@
+import { serverQueryContent } from '#content/server'
+import { SitemapStream, streamToPromise } from 'sitemap'
+export default defineEventHandler(async (event) => {
+    const docs = await serverQueryContent(event).find()
+    const sitemap = new SitemapStream({
+        hostname: 'https://eventuallycoding.com'
+    })
+    for (const doc of docs) {
+        if (doc._path !== '/resources/resources') {
+            sitemap.write({
+                url: doc._path.replace('/articles', ''),
+            })
+        }
+    }
+    sitemap.write({
+        url: '/resources',
+    })
+    sitemap.end()
+    return streamToPromise(sitemap)
+})
