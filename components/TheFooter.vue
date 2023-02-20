82ecef4af06fe09d7b63a654e2dfc5fe7474d5ac
hlassiege <hugo@malt.com>
2023-02-20 22:49:49 +0100

    Accessibility

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,54 +3,64 @@
         <div class="flex flex-col items-center mt-16">
             <div class="flex mb-3 space-x-4">
                 <a
+                    aria-label="Open RSS feed"
                     target="_self"
                     :href="siteMetadata.rss"
                     class="text-sm text-gray-500 transition hover:text-gray-600"
                 >
                     <img
+                        alt="RSS"
                         class="transition-transform hover:scale-110 w-8 h-8"
                         src="~assets/icon/rss.svg"
                     />
                 </a>
                 <a
+                    aria-label="Open Mastodon profile"
                     class="text-sm text-gray-500 transition hover:text-gray-600"
                     target="_blank"
                     rel="me"
                     :href="siteMetadata.mastodon"
                     ><span class="sr-only">Mastodon</span>
                     <img
+                        alt="Mastodon"
                         class="transition-transform hover:scale-110 w-8 h-8"
                         src="~assets/icon/mastodon.svg"
                     />
                 </a>
                 <a
+                    aria-label="Open github profile"
                     class="text-sm text-gray-500 transition hover:text-gray-600"
                     target="_blank"
                     rel="nofollow noopener noreferrer"
                     :href="siteMetadata.github"
                     ><span class="sr-only">github</span>
                     <img
+                        alt="github"
                         class="transition-transform hover:scale-110 w-8 h-8"
                         src="~assets/icon/github_new.svg"
                     /> </a
                 ><a
+                    aria-label="Open linkedin profile"
                     class="text-sm text-gray-500 transition hover:text-gray-600"
                     target="_blank"
                     rel="noopener noreferrer"
                     :href="siteMetadata.linkedin"
                     ><span class="sr-only">Linkedin</span>
                     <img
+                        alt="Linkedin"
                         class="transition-transform hover:scale-110 w-8 h-8"
                         src="~assets/icon/linkeding.svg"
                     />
                 </a>
                 <a
+                    aria-label="Open twitter profile"
                     target="_blank"
                     rel="nofollow noopener noreferrer"
                     :href="siteMetadata.twitter + siteMetadata.twitter_user"
                     class="text-sm text-gray-500 transition hover:text-gray-600"
                 >
                     <img
+                        alt="Twitter"
                         class="transition-transform hover:scale-110 w-8 h-8"
                         src="~assets/icon/twitter.svg"
                     />
